006cc5ca444a193e9bc76a52f4811c02f536c3c2
anamfazal <anamfazal94@gmail.com>
2020-10-29 02:16:18 +0530

    Added: test case to add multiple nodes in tree and check final size

--- src/test/java/com/binarytreeproject/MyBinaryTreeTest.java
+++ src/test/java/com/binarytreeproject/MyBinaryTreeTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MyBinaryTreeTest {
+
     @Test
     public void given3Numbers_WhenAddedToBinaryTreeShouldReturnSizeThree() {
         MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
@@ -11,6 +12,26 @@ public class MyBinaryTreeTest {
         myBinaryTree.add(30);
         myBinaryTree.add(70);
         int size = myBinaryTree.getSize();
-        Assert.assertEquals(3 , size);
+        Assert.assertEquals(3, size);
+    }
+
+    @Test
+    public void given13Numbers_WhenAddedToBinaryTreeShouldReturnBSTSizeThirteen() {
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        myBinaryTree.add(22);
+        myBinaryTree.add(11);
+        myBinaryTree.add(3);
+        myBinaryTree.add(40);
+        myBinaryTree.add(16);
+        myBinaryTree.add(60);
+        myBinaryTree.add(65);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
+        myBinaryTree.add(95);
+        int size = myBinaryTree.getSize();
+        Assert.assertEquals(13 , size);
     }
 }
